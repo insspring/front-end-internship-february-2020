287ba5b1abf8388639a22767e13fbd19d62857f6
zombine <xxlcrackerxxl@gmail.com>
2020-01-29 12:08:46 +0300

    added i18n

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Header></Header>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "../Header";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+      Header,
+      HelloWorld
   }
 };
 </script>
